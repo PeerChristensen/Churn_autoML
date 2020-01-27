52c9743a7e0a215f7ea83a570df04b7c4266de43
Peer Christensen (admin) <pech_adm@saxo.local>
2020-01-27 14:35:58 +0100

    missingness update

--- test_missingness.R
+++ test_missingness.R
@@ -18,7 +18,15 @@ close(channel)
 
 md.pattern(df)
 
-missing_churn <- md.pairs(df)$rr[2,] # rr (response-response, non-missing), row 2 is churn var
+observed_churn <- md.pairs(df)$rr[2,] # rr (response-response, non-missing), row 2 is churn var
+
+missing_churn <- md.pairs(df)$rm[2,] # rm (response-missing)
+
+#churn == 1
+df2 <- df %>% filter(Churned30==1)
+missing_churn2 <- md.pairs(df2)$rm[2,] # rm (response-missing)
+missing_cols <- missing_churn2[missing_churn2>0]
+missing_col_names <- attributes(missing_cols)$names
 
 library(VIM)
 nhanes_aggr = aggr(df, col=mdc(1:2), 
